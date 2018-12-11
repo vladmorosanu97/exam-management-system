630b1b0bb90d150b59a64833dba6c3737a33551a
Morosanu Vladut <vladmorosanu97@gmail.com>
2018-12-11 23:16:30 +0200

    added blModels

---
+++ ExamsSystem.BusinessLogic.Models/ClassroomBlModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamsSystem.BusinessLogic.Models
+{
+    public class ClassroomBlModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ExamsSystem.BusinessLogic.Models/ProfessorBlMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamsSystem.BusinessLogic.Models
+{
+    public class ProfessorBlMode
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<CourseBlModel> Courses { get; set; }
+    }
+}
